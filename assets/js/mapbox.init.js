93f9895a184fd86559f237c0143829182e23ab1b
Mohammad Mahdi Samei <9259samei@gmail.com>
2022-11-26 20:13:49 +0330

    Map Fixing

--- assets/js/mapbox.init.js
+++ assets/js/mapbox.init.js
@@ -2,7 +2,7 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiYXJ0dGVtcGxhdGUiLCJhIjoiY2s0M3I5ZHgzMGEzNDNuc
 var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/dark-v10',
-	center: [-73.9751,40.7289], // starting position
+	center: [36.32504,59.54205], // starting position
     zoom: 13 // starting zoom
 });
 
@@ -15,7 +15,7 @@ map.on('load', function () {
 
 // create the popup
 var popup = new mapboxgl.Popup({ offset: 40 }).setText(
-    '756 Livingston Street, Brooklyn, NY 11201'
+    'Azadi 4, Azadi, Mashhad, Mashhad County, Razavi Khorasan, 91886-13335, Iran'
 );
 
 // create DOM element for the marker
@@ -24,7 +24,7 @@ el.id = 'marker';
  
 // create the marker
 new mapboxgl.Marker(el)
-    .setLngLat([-73.9751,40.7289])
+    .setLngLat([36.32504,59.54205])
     .setPopup(popup) // sets a popup on this marker
     .addTo(map);
 
